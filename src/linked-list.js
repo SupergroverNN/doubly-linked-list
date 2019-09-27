4ad0d3891392ff16c22e70be7cdee922dd895568
shipulinia <shipulinia@lad24.ru>
2019-09-27 16:39:34 +0300

    feat: add half function

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,87 @@
-const Node = require('./node');
+const Node = require("./node");
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0;
+    this._head = null;
+    this._tail = null;
+  }
 
-    append(data) {}
+  append(data) {
+    let node = new Node(data);
+    if (!this._head) {
+      this._head = node;
+      this._tail = node;
+    } else {
+      node.prev = this._tail;
+      this._tail.next = node;
+      this._tail = node;
+    }
+    this.length++;
+    // console.log("_tail", this._tail);
+    // console.log("_head", this._head);
+    // console.log("----------------------");
+  }
 
-    head() {}
+  head() {
+    return this._head !== null ? this._head.data : null;
+  }
 
-    tail() {}
+  tail() {
+    return this._tail !== null ? this._tail.data : null;
+  }
 
-    at(index) {}
+  at(index) {
+    let count = 0;
+    let current = this._head;
+    if (index === 0 && this._head) {
+      return this._head.data;
+    }
+    while (current !== null && count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current.data;
+  }
 
-    insertAt(index, data) {}
+  insertAt(index, data) {
+    let count = 1;
+    let current = this._head;
+    let node = new Node(data);
+    if (index === 0) {
+      this._head.prev = node;
+      node.next = this._head;
+      this._head = node;
+    } else {
+      while (count > 0) {
+        current = current.next;
+        if (count === index) {
+          node.prev = current.prev;
+          current.prev.next = node;
+          node.next = current;
+          current.prev = node;
+          break;
+        }
+        count++;
+      }
+    }
+  }
 
-    isEmpty() {}
+  isEmpty() {
+    return this._head === null;
+  }
 
-    clear() {}
+  clear() {
+    this._head = null;
+    this._tail = null;
+    this.length = 0;
+  }
 
-    deleteAt(index) {}
+  deleteAt(index) {}
 
-    reverse() {}
+  reverse() {}
 
-    indexOf(data) {}
+  indexOf(data) {}
 }
 
 module.exports = LinkedList;
